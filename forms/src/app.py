288727252f3ab4c56340339b35eeed063f5b3008
eulauragabriel <eulauragabriel@gmail.com>
2023-04-26 22:25:52 -0300

    retirei o venv e pycache

--- forms/src/app.py
+++ forms/src/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def insira():
     return render_template("form.html")
 
-@app.route('/formula', methods=['POST', 'GET'])
+
+@app.route("/formula", methods=["POST", "GET"])
 def formula():
-    if request.method == 'POST':
-        x = int(request.form['x'])
-        y = int(request.form['y'])
-        z = int(request.form['z'])
+    if request.method == "POST":
+        x = int(request.form["x"])
+        y = int(request.form["y"])
+        z = int(request.form["z"])
         w = x - y
-        return str(f'Você fez/fará {z - x} anos em {z}, seu colega fez/fará {z - y} anos em {z} e vocês possuem {abs(w)} ano(s) de diferença!')
+        return str(
+            f"Você fez/fará {z - x} anos em {z}, seu colega fez/fará {z - y} anos em {z} e vocês possuem {abs(w)} ano(s) de diferença!"
+        )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
